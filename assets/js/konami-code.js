2a9be21d0ba9f5a8fd4271be23b17dd45a6e3f65
Ed <jr-lol@live.com>
2020-09-12 20:24:36 -0300

    konami-code

---
+++ assets/js/konami-code.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const profilePicture = document.querySelector(".title img");
+  const body = document.querySelector("body");
+  let keypressed = "";
+  let secretCode = "drunk";
+
+  window.addEventListener("keyup", function (e) {
+    keypressed += e.key;
+    if (keypressed.includes(secretCode) && e.key === "Enter") {
+      profilePicture.src = "images/drunk.png";
+      const audio = document.createElement("audio");
+      const source = document.createElement("source");
+      source.src = "audio/chuli-leto.mp3";
+      audio.appendChild(source);
+      body.appendChild(audio);
+      audio.currentTime = 107;
+      audio.play();
+
+      setInterval(function () {
+        if (document.documentElement.getAttribute("data-theme") === "light") {
+          setTheme("dark");
+        } else {
+          setTheme("light");
+        }
+      }, 300);
+    }
+  });
+});
