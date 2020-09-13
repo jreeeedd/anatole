0821666cc99d02c45327c518cbb628e8e0e9bdb4
Ed <jr-lol@live.com>
2020-09-12 21:47:18 -0300

    voltando ao normal

--- assets/js/konami-code.js
+++ assets/js/konami-code.js
@@ -4,25 +4,25 @@ document.addEventListener("DOMContentLoaded", function () {
   let keypressed = "";
   let secretCode = "drunk";
 
-  window.addEventListener("click", function (e) {
+  window.addEventListener("keyup", function (e) {
     keypressed += e.key;
-    //if (keypressed.includes(secretCode) && e.key === "Enter") {
-    profilePicture.src = "images/drunk.png";
-    const audio = document.createElement("audio");
-    const source = document.createElement("source");
-    source.src = "audio/chuli-leto.mp3";
-    audio.appendChild(source);
-    body.appendChild(audio);
-    audio.currentTime = 107;
-    audio.play();
+    if (keypressed.includes(secretCode) && e.key === "Enter") {
+      profilePicture.src = "images/drunk.png";
+      const audio = document.createElement("audio");
+      const source = document.createElement("source");
+      source.src = "audio/chuli-leto.mp3";
+      audio.appendChild(source);
+      body.appendChild(audio);
+      audio.currentTime = 107;
+      audio.play();
 
-    setInterval(function () {
-      if (document.documentElement.getAttribute("data-theme") === "light") {
-        setTheme("dark");
-      } else {
-        setTheme("light");
-      }
-    }, 300);
-    //  }
+      setInterval(function () {
+        if (document.documentElement.getAttribute("data-theme") === "light") {
+          setTheme("dark");
+        } else {
+          setTheme("light");
+        }
+      }, 300);
+    }
   });
 });
