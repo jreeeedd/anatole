f1ed39c8fede98ec3f18fd9f6fc78411e9342681
Ed <jr-lol@live.com>
2020-09-19 13:29:05 -0300

    refatorado código do konami

--- assets/js/konami-code.js
+++ assets/js/konami-code.js
@@ -4,25 +4,43 @@ document.addEventListener("DOMContentLoaded", function () {
   let keypressed = "";
   let secretCode = "drunk";
 
-  window.addEventListener("keyup", function (e) {
-    keypressed += e.key;
-    if (keypressed.includes(secretCode) && e.key === "Enter") {
-      profilePicture.src = "images/drunk.png";
-      const audio = document.createElement("audio");
-      const source = document.createElement("source");
-      source.src = "audio/chuli-leto.mp3";
-      audio.appendChild(source);
-      body.appendChild(audio);
-      audio.currentTime = 107;
-      audio.play();
+  function secretCodeMatch(e){
+    return keypressed.includes(secretCode) && e.key === "Enter";
+  }
 
-      setInterval(function () {
-        if (document.documentElement.getAttribute("data-theme") === "light") {
-          setTheme("dark");
-        } else {
-          setTheme("light");
-        }
-      }, 300);
+  function setAudio(){
+    const audio = document.createElement("audio");
+    const source = document.createElement("source");
+    source.src = "/audio/chuli-leto.mp3";
+    audio.appendChild(source);
+    body.appendChild(audio);
+    audio.currentTime = 107;
+    audio.play();
+  }
+
+  function flashLights(){
+    setInterval(function () {
+      if (document.documentElement.getAttribute("data-theme") === "light") {
+        setTheme("dark");
+      } else {
+        setTheme("light");
+      }
+    }, 300);
+  }
+
+  function changeProfilePicture(){
+    profilePicture.src = "/images/drunk.png";
+  }
+
+  function getSecretCode(e){
+    keypressed += e.key;
+
+    if (secretCodeMatch(e)) {
+      changeProfilePicture();
+      setAudio();
+      flashLights();
     }
-  });
+  }
+
+  window.addEventListener("keyup", getSecretCode);
 });
